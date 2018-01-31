5006b2a2e6944347f296fe24095524e44a9c34fa
vinod66 <35687613+vinod66@users.noreply.github.com>
2018-01-31 16:48:07 +0530

    OD
    OD

---
+++ od.C
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <conio.h>
+   {
+   int a;
+    printf("Enter a");
+    scanf("%d", &a);
+
+    if (a % 2 == 0)
+    {
+    printf("The given number is EVEN");
+    }
+    else 
+    {
+    printf("The given number is ODD");
+    }
+    return 0:
+    }
