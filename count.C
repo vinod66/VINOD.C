cb241edccfd9d825aa873eb2b11324c106d0f8db
vinod66 <35687613+vinod66@users.noreply.github.com>
2018-02-05 16:21:44 +0530

    count

---
+++ count.C
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+int n;
+count=0;
+printf("Enter any number:");
+scanf("%d",&num);
+while(num!==0)
+{
+count++;
+num 1=0;
+}
+printf("total digits:%d",count);
+return o;
+}
