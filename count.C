bcb343dc7f9b439d8892f989c69c3dd2ffcbd6b1
vinod66 <35687613+vinod66@users.noreply.github.com>
2018-02-05 16:23:51 +0530

    Update count.C

--- count.C
+++ count.C
@@ -1,7 +1,9 @@
 #include<stdio.h>
+#include<conio.h>
 int main()
 {
 int n;
+  clrscr();
 count=0;
 printf("Enter any number:");
 scanf("%d",&num);
@@ -12,4 +14,5 @@ num 1=0;
 }
 printf("total digits:%d",count);
 return o;
+  getch();
 }
