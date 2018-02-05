73e810eaf55970db779436f396a0db801d0c7e8b
vinod66 <35687613+vinod66@users.noreply.github.com>
2018-02-05 16:11:35 +0530

    sum

---
+++ sum.C
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int i,n,sum=0;
+printf("Enter the postive integer:");
+scanf("%d",&n);
+for(i=0;i<=0;i++)
+{
+sum=+i;
+}
+printf("sum=%d",sum);
+return 0;
+getch();
+}
