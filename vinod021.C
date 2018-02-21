96fa290256fe9520b4118e1967a95b9861cf0f4e
VINODKUMAR <35687613+vinod66@users.noreply.github.com>
2018-02-21 10:23:34 +0000

    Create vinod021.C

---
+++ vinod021.C
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int n,i;
+clrscr();
+printf("enter the number",n);
+for(i=1;i<n;i++)
+{
+if(i%5=0)
+{
+printf("%d/n",i);
+}
+}
+getch();
+return 0;
+}
