b370268ff0a5a396bb2708f00aaf9cf122b1b560
VINODKUMAR <35687613+vinod66@users.noreply.github.com>
2018-03-05 16:01:55 +0530

    Create 5(3).C

---
+++ 5(3).C
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include<string.h>
+int main(void)
+  {
+	char a[50];
+	int i,count=1;
+  clrscr();
+	gets(a);
+	
+	for(i=0;a[i]!='\0';i++)
+	{
+		if(a[i] == ' ')
+		count++;
+	}
+	printf("%d",count);
+  }
