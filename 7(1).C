e0f444a49ad71cec0081c9ae09d98aa00b29eb9f
VINODKUMAR <35687613+vinod66@users.noreply.github.com>
2018-03-07 15:41:26 +0530

    Create 7(1).C

---
+++ 7(1).C
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include<string.h>
+int main(void) 
+{
+	char a[50];
+	int i,count=1;
+  clrscr();
+	gets(a);
+	
+	for(i=0;a[i]!='\0';i++)
+	{
+		if(a[i] == '.')
+		count++;
+	}
+	printf("%d",count);
+  getch();
+  }
