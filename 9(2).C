3c9f8102a59588b596d780dfafc7da67873cfd1c
VINODKUMAR <35687613+vinod66@users.noreply.github.com>
2018-03-09 13:17:04 +0530

    Create 9(2).C

---
+++ 9(2).C
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <conio.h>
+
+int main(void) 
+{
+	int i,temp;
+	
+	int a[10];
+	for(i=0;i<10;i++)
+	{
+		scanf("%d",&a[i]);
+	}
+	for(i=0;i<10;i++)
+	{
+		
+		
+			if(a[i]>a[i+1])
+			{
+				temp=a[i];
+				a[i]=a[i+1];
+				a[i+1]=temp;
+			}
+			
+		
+	
+	}
+	printf("%d",a[i]);
+  getch();
+}
