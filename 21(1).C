e616cb5a4abc7b874ebd8fefb958a081543e3fcc
VINODKUMAR <35687613+vinod66@users.noreply.github.com>
2018-03-21 16:00:08 +0530

    Create 21(1).C

---
+++ 21(1).C
@@ -0,0 +1,11 @@
+ #include<stdio.h>
+ #include<conio.h>
+    void main()
+    {
+    	int a=5,n,s;
+    	int b=4,c=5,d=6,e=7,f=8;
+    	n=b+c+d+e+f;
+    	s=n/5;
+    	printf("%d",s);
+      getch();
+    }
